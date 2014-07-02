ee2e1a37d95d56715c813825bf42ec5e7ccaf28a
benpeng.jiang <benpeng.jiang@ttpod.com>
2014-07-02 16:27:32 +0800

    [update]-研究了jsoup库，完善了ConnectionManager类

--- src/com/jamesdev/penguinphoto/data/ConnectionManager.java
+++ src/com/jamesdev/penguinphoto/data/ConnectionManager.java
@@ -1,6 +1,8 @@
 package com.jamesdev.penguinphoto.data;
 
+import com.jamesdev.penguinphoto.app.framework.BaseApplication;
 import org.jsoup.Connection;
+import org.jsoup.Jsoup;
 
 /**
  * Created by Administrator on 14-6-29.
@@ -9,10 +11,42 @@ public class ConnectionManager {
     private static final String TAG = ConnectionManager.class.getSimpleName();
 
     public static final String BASE_URL = "http://127.0.0.1";
+    public static final String ITEMS_URL = "/item?id=";
+    public static final String THREADS_URL = "/threads?id=";
+    public static final String SUBMISSIONS_URL = "/submitted?id=";
+    public static final String SUBMIT_URL = "/submit";
+    public static final String USER_AGENT = System.getProperty("http.agent");
     public static final int TIMEOUT_MILLIS = 40 * 1000;
 
-    public static Connection anonConnection(String baseUrlExtension) {
-
+    /** Connects to server using the cookie you've provided **/
+    public static Connection authConnect(String baseUrlExtension, String userCookie) {
+        return anonConnect(baseUrlExtension).cookie("user", userCookie);
     }
 
+    /** Connects to server with no user cookie authentication **/
+    public static Connection anonConnect(String baseUrlExtension) {
+        Connection conn = Jsoup.connect(ConnectionManager.BASE_URL + baseUrlExtension)
+                .timeout(TIMEOUT_MILLIS)
+                .userAgent(ConnectionManager.USER_AGENT);
+
+        UserPrefs prefs = new UserPrefs(BaseApplication.getApp().getApplicationContext());
+
+        boolean compress = prefs.getCompressData();
+        if (compress) {
+            conn.header("Accept-Encoding", "gzip");
+        }
+
+        return conn;
+    }
+
+
+    /** Converts an id into a string containing the extension (everything that goes after .com) of the URL **/
+    public static String itemIdToUrlExtension(long id) {
+        return ITEMS_URL + Long.toString(id);
+    }
+
+    /** Converts an id into a string the full URL for that id. **/
+    public static String itemIdToUrl(long id) {
+        return BASE_URL + itemIdToUrlExtension(id);
+    }
 }
