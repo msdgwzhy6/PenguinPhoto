2cd33c19c16c1457b857f9efe6e2baa873123729
bigconvience <jbp.130@163.com>
2014-07-02 22:53:44 +0800

    [update]-完善了登陆的功能，接下来准备进行测试

--- src/com/jamesdev/penguinphoto/activities/LoginActivity.java
+++ src/com/jamesdev/penguinphoto/activities/LoginActivity.java
@@ -8,7 +8,11 @@ import android.support.v4.content.Loader;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import com.jamesdev.penguinphoto.R;
+import com.jamesdev.penguinphoto.data.UserPrefs;
+import com.jamesdev.penguinphoto.loader.LoginLoader;
+import com.jamesdev.penguinphoto.model.Result;
 
 /**
  * Created by Administrator on 14-6-28.
@@ -23,6 +27,9 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
     private String mUsername;
     private String mPassword;
 
+    public enum LoginResult {
+        EMPTY, SUCCESS, FAILURE;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,12 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
         setContentView(R.layout.activity_login);
 
         findViews();
+
+        UserPrefs userData = new UserPrefs(this);
+        if (userData.isLoggedIn()) {
+            Toast.makeText(this, "Already logged int", Toast.LENGTH_SHORT).show();
+            finish();
+        }
 
         getSupportLoaderManager().initLoader(0, null, this);
     }
@@ -79,16 +92,34 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
 
     @Override
     public Loader<Result> onCreateLoader(int id, Bundle args) {
-
+        return new LoginLoader(this, mUsername, mPassword);
     }
 
     @Override
     public void onLoadFinished(Loader<Result> loader, Result result) {
+        if (result == Result.EMPTY) {
+            return;// this means the request was from initLoader()
+        }
 
+        if (result == Result.SUCCESS) {
+            Toast.makeText(this, getString(R.string.logging_in_), Toast.LENGTH_SHORT).show();
+            //TODO here
+            finish();
+        } else {
+            showError();
+            Toast.makeText(this, getString(R.string.login_failure), Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
     public void onLoaderReset(Loader<Result> loader) {
         // No implementation necessary
+    }
+
+    private void showError() {
+        if (mEditUsername != null) mEditUsername.setEnabled(true);
+        if (mEditPassword != null) mEditPassword.setEnabled(true);
+        mLoginButton.setVisibility(View.VISIBLE);
+        mLoginIndicator.setVisibility(View.GONE);
     }
 }
--- src/com/jamesdev/penguinphoto/loader/LoginLoader.java
+++ src/com/jamesdev/penguinphoto/loader/LoginLoader.java
@@ -3,6 +3,8 @@ package com.jamesdev.penguinphoto.loader;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v4.content.AsyncTaskLoader;
+import com.jamesdev.penguinphoto.data.LoginManager;
+import com.jamesdev.penguinphoto.data.UserPrefs;
 import com.jamesdev.penguinphoto.model.Result;
 
 /**
@@ -22,7 +24,19 @@ public class LoginLoader extends AsyncTaskLoader<Result> {
     public Result loadInBackground() {
         if (mUsername == null || mPassword == null) return Result.EMPTY;
 
+        String newCookie = LoginManager.login(mUsername, mPassword);
+        boolean isSuccess = newCookie != null;
 
+        if (isSuccess) {
+            UserPrefs prefs = new UserPrefs(getContext());
+            prefs.saveUserCookie(newCookie);
+            prefs.saveUsername(mUsername);
+            prefs.savePassword(mPassword);
+
+            //TODO: update all caches
+        }
+
+        return isSuccess ? Result.SUCCESS : Result.FAILURE;
     }
 
 
