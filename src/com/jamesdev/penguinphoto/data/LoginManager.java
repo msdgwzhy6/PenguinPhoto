2eb9dd59c807341e7d2db185136f12066ab20c6d
benpeng.jiang <benpeng.jiang@ttpod.com>
2014-07-02 19:54:38 +0800

    [update]-添加了commons-lang包

--- src/com/jamesdev/penguinphoto/data/LoginManager.java
+++ src/com/jamesdev/penguinphoto/data/LoginManager.java
@@ -1,10 +1,45 @@
 package com.jamesdev.penguinphoto.data;
 
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Connection;
+import org.jsoup.nodes.Document;
+
 /**
  * Created by Administrator on 14-6-29.
  */
 public class LoginManager {
-    private static final String LOGIN_URL = "/login";
+    private static final String LOGIN_URL_EXTENSION = "/login";
 
+    /**
+     * Attempts to log the user into http://news.ycombinator.com. If successful, returns a user authentication cookie.
+     * Else it returns null.
+     **/
+    public static String login(String username, String password) {
+        try {
+            Connection.Response loginResponse = ConnectionManager.anonConnect(LOGIN_URL_EXTENSION)
+                    .method(Connection.Method.GET)
+                    .execute();
+            Document loginPage = loginResponse.parse();
+            String fnid = loginPage.select("input[name=fnid]")
+                    .attr("value");
 
+            Connection.Response response = ConnectionManager.anonConnect("/y")
+                    .data("fnid", fnid)
+                    .data("u", username)
+                    .data("p", password)
+                    .header("Origin", ConnectionManager.BASE_URL)
+                    .followRedirects(true)
+                    .referrer(ConnectionManager.BASE_URL + LOGIN_URL_EXTENSION)
+                    .method(Connection.Method.POST)
+                    .execute();
+
+            String cookie = response.cookie("user");
+            if (StringUtils.isNotBlank(cookie)) {
+                return cookie;
+            }
+        } catch (Exception e) {
+            // connection error
+        }
+        return null;
+    }
 }
