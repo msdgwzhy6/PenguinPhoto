e90750613e4ed48cde53b08145f1053a8f344b38
benpeng.jiang <benpeng.jiang@ttpod.com>
2014-07-08 09:34:24 +0800

    [update]-重构登陆代码

--- src/com/jamesdev/penguinphoto/data/ConnectionManager.java
+++ src/com/jamesdev/penguinphoto/data/ConnectionManager.java
@@ -1,8 +1,20 @@
 package com.jamesdev.penguinphoto.data;
 
-import com.jamesdev.penguinphoto.app.framework.BaseApplication;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Administrator on 14-6-29.
@@ -18,27 +30,6 @@ public class ConnectionManager {
     public static final String USER_AGENT = System.getProperty("http.agent");
     public static final int TIMEOUT_MILLIS = 40 * 1000;
 
-    /** Connects to server using the cookie you've provided **/
-    public static Connection authConnect(String baseUrlExtension, String userCookie) {
-        return anonConnect(baseUrlExtension).cookie("user", userCookie);
-    }
-
-    /** Connects to server with no user cookie authentication **/
-    public static Connection anonConnect(String baseUrlExtension) {
-        Connection conn = Jsoup.connect(ConnectionManager.BASE_URL + baseUrlExtension)
-                .timeout(TIMEOUT_MILLIS)
-                .userAgent(ConnectionManager.USER_AGENT);
-
-        UserPrefs prefs = new UserPrefs(BaseApplication.getApp().getApplicationContext());
-
-        boolean compress = prefs.getCompressData();
-        if (compress) {
-            conn.header("Accept-Encoding", "gzip");
-        }
-
-        return conn;
-    }
-
 
     /** Converts an id into a string containing the extension (everything that goes after .com) of the URL **/
     public static String itemIdToUrlExtension(long id) {
@@ -48,5 +39,58 @@ public class ConnectionManager {
     /** Converts an id into a string the full URL for that id. **/
     public static String itemIdToUrl(long id) {
         return BASE_URL + itemIdToUrlExtension(id);
+    }
+
+    public static void doPost(String urlExtension, Map<String, String> parmas){
+        ArrayList<BasicNameValuePair> pairs = convertMapToNameValuePair(parmas);
+
+        try {
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(BASE_URL + urlExtension);
+            UrlEncodedFormEntity p_entity = new UrlEncodedFormEntity(pairs, "utf-8");
+            httpPost.setEntity(p_entity);
+
+            HttpResponse response = client.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+            InputStream content = entity.getContent();
+            String returnConnection = convertStreamToString(content);
+            System.out.print(returnConnection);
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static ArrayList<BasicNameValuePair> convertMapToNameValuePair(Map<String, String> parmas) {
+        ArrayList<BasicNameValuePair> pairs = new ArrayList<BasicNameValuePair>();
+        if(parmas != null){
+            Set<String> keys = parmas.keySet();
+            for(Iterator<String> i = keys.iterator(); i.hasNext();) {
+                String key = i.next();
+                pairs.add(new BasicNameValuePair(key, parmas.get(key)));
+            }
+        }
+        return pairs;
+    }
+
+    private static String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
     }
 }
--- src/com/jamesdev/penguinphoto/data/LoginManager.java
+++ src/com/jamesdev/penguinphoto/data/LoginManager.java
@@ -1,31 +1,13 @@
 package com.jamesdev.penguinphoto.data;
 
-import com.jamesdev.penguinphoto.model.Photo;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.CoreProtocolPNames;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
-import org.jsoup.Connection;
-import org.jsoup.nodes.Document;
-
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URLEncoder;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Administrator on 14-6-29.
  */
 public class LoginManager {
-    private static final String LOGIN_URL_EXTENSION = "/accounts/login";
+    private static final String LOGIN_URL_EXTENSION = "/accounts/login/";
     private static final String CSRF_TOKEN = "csrfmiddlewaretoken";
     private static final String COOKIE_CSRF_TOKEN = "csrftoken";
     private static final String HEADER_CSRF_TOKEN = "X-CSRFToken";
@@ -39,95 +21,14 @@ public class LoginManager {
      **/
     public static String login(String username, String password) {
         try {
-            Connection.Response loginResponse = ConnectionManager.anonConnect(LOGIN_URL_EXTENSION)
-                    .method(Connection.Method.GET)
-                    .execute();
+            Map<String, String> parmas = new HashMap<String, String>();
+            parmas.put("username", username);
+            parmas.put("password", password);
 
-/*            Connection.Response response = ConnectionManager.anonConnect(LOGIN_URL_EXTENSION + "/")
-                    .data(USERNAME, username)
-                    .data(PASSWORD, password)
-                    .header("Origin", ConnectionManager.BASE_URL)
-                    .followRedirects(true)
-                    .referrer(ConnectionManager.BASE_URL + LOGIN_URL_EXTENSION)
-                    .method(Connection.Method.POST)
-                    .execute();*/
-
-            dopost(username, password, loginResponse);
-
-/*            String cookie = response.cookie("user");
-            if (StringUtils.isNotBlank(cookie)) {
-                return cookie;
-            }*/
+            ConnectionManager.doPost(LOGIN_URL_EXTENSION, parmas);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
-    }
-
-    private static void dopost(String username, String password, Connection.Response loginResponse){
-        String csrfToken = loginResponse.cookie(COOKIE_CSRF_TOKEN);
-        String sessionId = loginResponse.cookie(SESSION_ID);
-        //封装数据
-        Map<String, String> parmas = new HashMap<String, String>();
-        parmas.put("username", username);
-        parmas.put("password", password);
-        parmas.put(CSRF_TOKEN, csrfToken);
-
-        DefaultHttpClient client = new DefaultHttpClient();//http客户端
-        HttpPost httpPost = new HttpPost(ConnectionManager.BASE_URL + LOGIN_URL_EXTENSION + "/");
-
-        ArrayList<BasicNameValuePair> pairs = new ArrayList<BasicNameValuePair>();
-        if(parmas != null){
-            Set<String> keys = parmas.keySet();
-            for(Iterator<String> i = keys.iterator(); i.hasNext();) {
-                String key = (String)i.next();
-                pairs.add(new BasicNameValuePair(key, parmas.get(key)));
-            }
-        }
-
-        try {
-            UrlEncodedFormEntity p_entity = new UrlEncodedFormEntity(pairs, "utf-8");
-         /*
-          *  将POST数据放入HTTP请求
-          */
-            httpPost.setEntity(p_entity);
-          //  httpPost.addHeader("User-Agent", ConnectionManager.USER_AGENT);
-      //      httpPost.addHeader("Set-Cookie", COOKIE_CSRF_TOKEN + "=" + csrfToken + ";" + SESSION_ID + "=" + sessionId);
-
-         /*
-          *  发出实际的HTTP POST请求
-           */
-            client.getParams().setParameter(CoreProtocolPNames.USER_AGENT,  ConnectionManager.USER_AGENT);
-            HttpResponse response = client.execute(httpPost);
-            HttpEntity entity = response.getEntity();
-            InputStream content = entity.getContent();
-            String returnConnection = convertStreamToString(content);
-            System.out.print(returnConnection);
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private static String convertStreamToString(InputStream is) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return sb.toString();
     }
 }
